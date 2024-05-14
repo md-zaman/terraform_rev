665bfa16c3fd4c47185bbb156742d89958d10474
Md Zaman <md.zaman4493@gmail.com>
2024-05-15 00:58:36 +0530

    basic edit

--- Day-2/06-variables-implementation.tf
+++ Day-2/06-variables-implementation.tf
@@ -26,7 +26,8 @@ resource "aws_instance" "example_instance" {
   instance_type = var.instance_type
 }
 
-# Define an output variable to expose the public IP address of the EC2 instance
+# Define an output variable to expose the public IP address of the EC2 
+# instance
 output "public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.example_instance.public_ip
